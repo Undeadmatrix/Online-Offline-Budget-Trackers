f5b81ea5fac680148b2068c8a5d53ebb17b202e3
Chris Little <chrisdlittle@aol.com>
2020-06-18 13:29:02 -0400

    bug fixing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,64 +1,65 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/index.js",
-    "/db.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-  ];
-  
-  const CACHE_NAME = "my-site-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
-  self.addEventListener("install", function(event) {
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then(function(cache) {
-            console.log("opened cache");
-            return cache.addAll(FILES_TO_CACHE);
-        }
-    ));
-  });
-  
-  self.addEventListener("fetch", function(event) {
-    // non GET requests are not cached and requests to other origins are not cached
-    if (
-      event.request.url.includes("/api/")
-    ) {
-      event.respondWith(caches.open(DATA_CACHE_NAME)
-      .then(cache => {
-          return fetch(event.request)
-          .then(response => {
-              if(response.status === 200)
-              {
-                  cache.put(event.request.url, response.clone());
-              }
-              return response;
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+  const CACHE_NAME = 'static-cache-v13';
+  const DATA_CACHE_NAME = 'data-cache-v8';
+  //Install service worker
+  self.addEventListener('install', evt => {
+      evt.waitUntil(
+          caches.open(CACHE_NAME).then(cache =>{
+              console.log('Your files were pre-cached successfully');
+              return cache.addAll(FILES_TO_CACHE);
           })
-          .catch(err => {
-              return cache.match(event.request);
-          });  
-      })
-      .catch(err => console.log(err)));
-      return;
-    }
-  
-    // use cache first for all other requests for performance
-    event.respondWith(
-      fetch(event.request).catch(function() {
-          return caches.match(event.request)
-          .then(function(response) {
-              if(response)
-              {
-                  return response;
-              }
-              else if(event.request.headers.get("accept").includes("text/html"))
-              {
-                  return caches.match("/");
-              }
-          });
-        })
-    );
-});
+      );
+      self.skipWaiting();
+  });
+  // Activate Service Worker
+  self.addEventListener('activate', evt => {
+      evt.waitUntil(
+          caches.keys().then(keyList => {
+              return Promise.all(
+                  keyList.map( key => {
+                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                          console.log('Removing old cache data', key);
+                          return caches.delete(key);
+                      }
+                  })
+              );
+          })
+      );
+      self.clients.claim();
+  });
+  // 5. Fetch Files
+  self.addEventListener('fetch',  evt =>{
+      if (evt.request.url.includes('/api/')) {
+          console.log('[Service Worker] Fetch (data)', evt.request.url);
+          evt.respondWith(
+              caches.open(DATA_CACHE_NAME).then(cache => {
+                  return fetch(evt.request)
+                      .then(response  => {
+                          if (response.status === 200) {
+                              cache.put(evt.request.url, response.clone());
+                          }
+                          return response;
+                      })
+                      .catch(err => {
+                          return cache.match(evt.request);
+                      });
+              })
+          );
+          return;
+      }
+      evt.respondWith(
+          caches.open(CACHE_NAME).then( cache => {
+              return cache.match(evt.request).then(response => {
+                  return response || fetch(evt.request)
+              });
+          })
+      );
+  });
